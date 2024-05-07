7027c2979cff0ddecb73533396edc610801bc8ee
DevJoaoVitor2402 <devjoaovitornogueiramarques@gmail.com>
2024-05-07 16:21:50 -0300

    v.1.0.7

---
+++ src/components/pageTwo/index.tsx
@@ -0,0 +1,24 @@
+import KarateSushi from '../../assets/img/karateSushi.svg'
+
+function PageTwo(){
+
+    return(
+        <section className="bg-blue-950 flex items-center justify-center">
+            <main >
+                <div>
+                    <h2 className=" font-bold text-xl items-center text-center mt-11 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum reprehenderit eos itaque totam laborum consequuntur animi maxime repellendus, debitis cum atque molestiae sed porro consequatur voluptatum laboriosam? Commodi, veniam maiores.</h2>
+                </div>
+
+                <div className="flex items-center justify-center">
+            <img
+              src={KarateSushi}
+              alt="logo-da-rice-swap" 
+              className="mt-40 mb-10 md:w-1/2"
+            />
+          </div>
+            </main>
+        </section>
+
+    )
+}
+export default PageTwo 
--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,18 +1,18 @@
 import Header from "../../components/header"
-
+import PageTwo from "../../components/pageTwo"
 function Home() {
 
     return(
-        <div className="w-full bg-blue-950 h-20 ">
+        <div className="bg-blue-950 h-screen ">
             <Header/>
-            <main  className='bg-blue-950 -400 flex justify-center items-top h-screen max-w-full'>
+            <main  className=' bg-blue-950 flex justify-center items-top  max-w-full'>
                 <section>
                     <div className="mx-5">
                         <h1 className=" font-bold text-3xl  text-white max-md:text-2xl p-3 mx-5 max-md:mt-12 md: md:text-5xl sm:text-4xl md:my-16"> Lorem ipsum dolor sit amet <span className="text-blue-500">consectetur</span>!</h1>
                         
-                        <p className="text-gray-600  max-sm:p-3 mx-5 mb-20 md:text-3xl md:mt-20 sm:text-2xl sm:mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea incidunt odit cumque error illo dignissimos tenetur beatae nobis quam? Pariatur magnam eligendicumque error illo dignissimos tenetur beatae nobis quam? Pariatur magnam eligendicumque error illo dignissimos tenetur beatae nobis quam? Pariatur magnam eligendi.cumque error illo dignissimos tenetur beatae nobis quam? Pariatur magnam eligendi....</p>
+                        <p className="text-gray-600  max-sm:p-3 mx-5 mb-20 md:text-3xl md:mt-20 sm:text-2xl sm:mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea incidunt odit cumque error illo dignissimos tenetur beatae nobis quam? Pariatur magnam eligendicumque error illo dignissimos tenetur beatae nobis quam? Pariatur magnam eligendicumque ..</p>
                     </div>
-                   <section className=" bg-opacity-30 px-2 py-3 flex justify-center items-center bg-slate-500  mx-3 mt-2  gap-11 rounded-xl shadow-2xl max-sm:gap-0 max-sm:mt-10 max-md:gap-40 max-md:mt-40 max-md:p-5 md:gap-20">
+                   <section className=" bg-opacity-30 px-2 py-3 flex justify-center items-center bg-slate-500  mx-3 mt-2  gap-11 rounded-xl shadow-2xl max-sm:gap-0 max-sm:mt-10 max-md:gap-40 max-md:mt-40 max-md:p-5 md:gap-20 mb-24">
                      
                         <article className="flex flex-col  items-center gap-2 mx-2 max-md:text-xs px-3 md: text-3xl">
                             <h2 className=" font-bold text-base text-white md:text-2xl">$1,00</h2>
@@ -30,6 +30,7 @@ function Home() {
                     </section>
                 </section>
             </main>
+            <PageTwo/>
         </div>
     )
 }
